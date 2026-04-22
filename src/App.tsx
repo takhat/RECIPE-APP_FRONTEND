cd854b6c4060760a3e61b21fc4153e88064bfdb1
Roshni <roshnipohuja@gmail.com>
2026-04-22 11:21:26 -0700

    add and display recipe-card components and 'ViewMore' button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import * as api from './api';
-import type { Recipe } from './types';
+import { type Recipe } from './types';
+import RecipeCard from './components/RecipeCard';
 
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState('lunch');
-  const [diet, setDiet] = useState('Lacto-Vegetarian');
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [diet] = useState('Lacto-Vegetarian');
   const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const pageNumber = useRef(1);
 
   // Function that gets called when the user submits the search term. 
   // It makes an API call to the backend to fetch recipes based on the search term and updates the state with the results.
@@ -20,19 +22,28 @@ const App = () => {
       console.log('Error fetching recipes:', err);
     }
   };
-  
+
+  const handleViewMoreClick = async () => {
+    const nextPage = pageNumber.current + 1; // Increment the page number
+    try {
+      const nextRecipes = await api.searchRecipes(searchTerm, nextPage, diet);
+      setRecipes([...recipes, ...nextRecipes.results]); // Append new recipes to existing ones
+    } catch (err) {
+      console.log('Error fetching more recipes:', err);
+    }
+  };
+
   return (
     <div>
       <form onSubmit={(event) => { handleSearchSubmit(event); }}>
+        <input type="text" required placeholder='Enter a search term ...' value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)} />
         <button type="submit">Submit</button>
       </form>
 
       {recipes?.map((recipe) => (
-        <div>
-          recipe image location: {recipe.image}
-          recipe.title: {recipe.title}
-        </div>
+        <RecipeCard recipe={recipe}/>
       ))}
+      <button className='view-more-button' onClick={handleViewMoreClick}>View More</button>
     </div>
   );
 };
